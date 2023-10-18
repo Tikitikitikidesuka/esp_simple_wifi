83cd8d5a1c9587b3fb6c6be9435cbf53ee36aada
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-10-18 12:29:12 +0200

    Add missing include to sta src

--- components/esp_ez_wifi/esp_ez_wifi_sta.c
+++ components/esp_ez_wifi/esp_ez_wifi_sta.c
@@ -3,6 +3,9 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
 #include "esp_log.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h"
